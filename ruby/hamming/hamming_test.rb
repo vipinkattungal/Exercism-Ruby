2818a8a22968c3d803e454292b2b9f74d3cc85e7
vipinkattungal <vipinkattungal@gmail.com>
2016-10-21 16:21:37 +0530

    this is a test case of hamming methode

--- ruby/hamming/hamming_test.rb
+++ ruby/hamming/hamming_test.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+	#!/usr/bin/env ruby
 gem 'minitest', '>= 5.0.0'
 require 'minitest/autorun'
 require_relative 'hamming'
@@ -13,72 +13,72 @@ class HammingTest < Minitest::Test
   end
 
   def test_long_identical_strands
-    skip
+    #skip
     assert_equal 0, Hamming.compute('GGACTGA', 'GGACTGA')
   end
 
   def test_complete_distance_in_single_nucleotide_strands
-    skip
+    #skip
     assert_equal 1, Hamming.compute('A', 'G')
   end
 
   def test_complete_distance_in_small_strands
-    skip
+    #skip
     assert_equal 2, Hamming.compute('AG', 'CT')
   end
 
   def test_small_distance_in_small_strands
-    skip
+    #skip
     assert_equal 1, Hamming.compute('AT', 'CT')
   end
 
   def test_small_distance
-    skip
+   # skip
     assert_equal 1, Hamming.compute('GGACG', 'GGTCG')
   end
 
   def test_small_distance_in_long_strands
-    skip
+    #skip
     assert_equal 2, Hamming.compute('ACCAGGG', 'ACTATGG')
   end
 
   def test_non_unique_character_in_first_strand
-    skip
+    #skip
     assert_equal 1, Hamming.compute('AGA', 'AGG')
   end
 
   def test_non_unique_character_in_second_strand
-    skip
+    #skip
     assert_equal 1, Hamming.compute('AGG', 'AGA')
   end
 
   def test_same_nucleotides_in_different_positions
-    skip
+    #skip
     assert_equal 2, Hamming.compute('TAG', 'GAT')
   end
 
   def test_large_distance
-    skip
+    #skip
     assert_equal 4, Hamming.compute('GATACA', 'GCATAA')
   end
 
   def test_large_distance_in_off_by_one_strand
-    skip
+    #skip
     assert_equal 9, Hamming.compute('GGACGGATTCTG', 'AGGACGGATTCT')
   end
 
   def test_empty_strands
-    skip
+    #skip
     assert_equal 0, Hamming.compute('', '')
   end
 
   def test_disallow_first_strand_longer
-    skip
+    #skip
     assert_raises(ArgumentError) { Hamming.compute('AATG', 'AAA') }
   end
 
   def test_disallow_second_strand_longer
-    skip
+    #skip
     assert_raises(ArgumentError) { Hamming.compute('ATA', 'AGTG') }
   end
 
@@ -99,7 +99,7 @@ class HammingTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
-    skip
+   # skip
     assert_equal 3, BookKeeping::VERSION
   end
 end
