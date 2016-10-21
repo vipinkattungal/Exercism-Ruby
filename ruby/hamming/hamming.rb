323bbcf6e6efdc93d48f975aa1951d8e9cd53323
vipinkattungal <vipinkattungal@gmail.com>
2016-10-21 16:20:56 +0530

    hamming program created

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,2 +1,18 @@
-class hamming
+module BookKeeping
+VERSION = 3
 end
+class Hamming
+	def self.compute(string1,string2)
+		raise ArgumentError if string1.length != string2.length
+        count = 0
+        (0..string1.length).select do |i| 
+			if string1[i] != string2[i]
+				count += 1
+			end	
+		end
+		count	
+	end	
+end
+
+
+
