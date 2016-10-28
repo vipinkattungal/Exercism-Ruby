61b2b7b393d3ad75bf6de0e6e0cf03e035fdbee2
vipinkattungal <vipinkattungal@gmail.com>
2016-10-28 18:55:53 +0530

    rna transcrption program

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,12 +1,9 @@
-class Complement
-def self.of_dna(string1)
-#	a =
-
-string1.gsub(/[GCTA]/,'G' => 'C', 'C' => 'G','T' => 'A','A' => 'U')
-
-ret = ""
-   string1.each_char { |x| ret << of_dna(x) }
-   ret
-
+module BookKeeping
+VERSION =  4
 end
+class Complement
+  def self.of_dna(string1)
+
+    string1 =~ /[^CGTA]/ ? '': string1.tr('CGTA','GCAU')
+  end
 end
