b02ce0c50e5a6ff48b6127c7a4c8111f866111be
vipinkattungal <vipinkattungal@gmail.com>
2016-11-01 18:57:17 +0530

    hamming program indendation changed

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,9 +1,9 @@
 module BookKeeping
-VERSION = 3
+VERSION =  3
 end
 class Hamming
-	def self.compute(string1,string2)
-		raise ArgumentError if string1.length != string2.length
+def  self.compute(string1, string2)
+	raise ArgumentError if string1.length != string2.length
         count = 0
         (0..string1.length).select do |i| 
 			if string1[i] != string2[i]
