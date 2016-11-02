c0808a6eb257a8103039d6ae61d6ef8bc99461e5
vipinkattungal <vipinkattungal@gmail.com>
2016-11-02 18:50:02 +0530

    ranidrops sample program test

---
+++ ruby/raindrops/raindrops2.rb
@@ -0,0 +1,14 @@
+class Raindrops
+def self.convert(number)
+if number % 3 == 0
+   "#{Pling}"
+elsif number % 5 == 0
+  "Plang"
+elsif number % 7 == 0
+ "Plong"
+else
+   "#{number}"
+
+end
+end
+end
