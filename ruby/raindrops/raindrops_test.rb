8c2acc117eb0c4f357256d448208dc64cb4e82ab
vipinkattungal <vipinkattungal@gmail.com>
2016-11-02 18:50:33 +0530

    raindrop test file

--- ruby/raindrops/raindrops_test.rb
+++ ruby/raindrops/raindrops_test.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+  #!/usr/bin/env ruby
 gem 'minitest', '>= 5.0.0'
 require 'minitest/autorun'
 require_relative 'raindrops'
@@ -11,87 +11,87 @@ class RaindropsTest < Minitest::Test
   end
 
   def test_3
-    skip
+    #skip
     assert_equal 'Pling', Raindrops.convert(3)
   end
 
   def test_5
-    skip
+    #skip
     assert_equal 'Plang', Raindrops.convert(5)
   end
 
   def test_7
-    skip
+    #skip
     assert_equal 'Plong', Raindrops.convert(7)
   end
 
   def test_6
-    skip
+    #skip
     assert_equal 'Pling', Raindrops.convert(6)
   end
 
   def test_8
-    skip
+    #skip
     assert_equal '8', Raindrops.convert(8)
   end
 
   def test_9
-    skip
+    #skip
     assert_equal 'Pling', Raindrops.convert(9)
   end
 
   def test_10
-    skip
+    #skip
     assert_equal 'Plang', Raindrops.convert(10)
   end
 
   def test_14
-    skip
+    #skip
     assert_equal 'Plong', Raindrops.convert(14)
   end
 
   def test_15
-    skip
+    #skip
     assert_equal 'PlingPlang', Raindrops.convert(15)
   end
 
   def test_21
-    skip
+    #skip
     assert_equal 'PlingPlong', Raindrops.convert(21)
   end
 
   def test_25
-    skip
+    #skip
     assert_equal 'Plang', Raindrops.convert(25)
   end
 
   def test_27
-    skip
+    #skip
     assert_equal 'Pling', Raindrops.convert(27)
   end
 
   def test_35
-    skip
+    #skip
     assert_equal 'PlangPlong', Raindrops.convert(35)
   end
 
   def test_49
-    skip
+    #skip
     assert_equal 'Plong', Raindrops.convert(49)
   end
 
   def test_52
-    skip
+    #skip
     assert_equal '52', Raindrops.convert(52)
   end
 
   def test_105
-    skip
+    #skip
     assert_equal 'PlingPlangPlong', Raindrops.convert(105)
   end
 
   def test_3125
-    skip
+    #skip
     assert_equal 'Plang', Raindrops.convert(3125)
   end
 
