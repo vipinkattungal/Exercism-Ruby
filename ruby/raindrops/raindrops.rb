917fbe3773a2439fed4db9cf68ff198b9026c837
vipinkattungal <vipinkattungal@gmail.com>
2016-11-02 18:49:23 +0530

    raindrops program created using .inject function

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,14 +1,11 @@
 class Raindrops
-def self.convert(number)
-a = number if number%3==0
-b = number if number%5==0
-c = number if number%7==0
- "Pling" if number.include?a
-"Plang" if number.include?b
-"Plong" if number.include?c
-if number!=a && number!=b && number!=c
-  puts number
-end
+  def self.convert number
+    words = [
+      *("Pling" if (number % 3).zero?),
+      *("Plang" if (number % 5).zero?),
+      *("Plong" if (number % 7).zero?)
+    ].inject(:+)
 
-end
+    words || number.to_s
+  end
 end
